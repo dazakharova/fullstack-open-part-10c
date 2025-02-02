daef172260e9d28be4bd8339bafc499cef410b52
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-02-02 16:15:16 +0200

    Implement sign-in form with Formik (ex. 10.8)

--- reviewed-repository-list-app/src/components/SignIn.jsx
+++ reviewed-repository-list-app/src/components/SignIn.jsx
@@ -1,7 +1,83 @@
 import Text from './Text';
+import { View, TextInput, Pressable } from 'react-native';
+import { useFormik } from 'formik';
+import theme from "../theme";
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    maxWidth: 300,
+    alignSelf: 'center',
+    backgroundColor: theme.backgroundColors.mainBackground,
+    borderRadius: 10,
+  },
+  formInput: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: theme.colors.textSecondary,
+    borderRadius: 5,
+    marginTop: 10,
+    backgroundColor: theme.backgroundColors.repositoryItemBackground,
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+    color: theme.colors.textPrimary,
+    marginHorizontal: 20,
+  },
+  submitButton: {
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginTop: 10,
+    backgroundColor: theme.colors.primary,
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  buttonText: {
+    color: theme.colors.appBarText,
+    fontWeight: theme.fontWeights.bold,
+    fontSize: theme.fontSizes.subheading,
+  },
+};
+
+const initialValues = {
+  username: '',
+  password: '',
+}
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  }
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+      <View stye={styles.container}>
+        <TextInput
+            style={styles.formInput}
+            placeholder="Username"
+            value={formik.values.username}
+            onChangeText={formik.handleChange('username')}
+        />
+        <TextInput
+            style={styles.formInput}
+            secureTextEntry={true}
+            placeholder="Password"
+            value={formik.values.password}
+            onChangeText={formik.handleChange('password')}
+        />
+        <Pressable style={styles.submitButton} onPress={formik.handleSubmit}>
+          <Text>Sign in</Text>
+        </Pressable>
+      </View>
+  )
 };
 
 export default SignIn;
