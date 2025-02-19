d7dbb6e2fc3c7394c97a849bc09b418221f0c58c
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-02-19 15:12:01 +0200

    Use environment variable for Apollo Client URI (ex. 10.12)

--- reviewed-repository-list-app/App.js
+++ reviewed-repository-list-app/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
 
 import { NativeRouter } from 'react-router-native';
 
@@ -9,6 +10,8 @@ import createApolloClient from './src/utils/apolloClient';
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
+
   return (
 
       <>
